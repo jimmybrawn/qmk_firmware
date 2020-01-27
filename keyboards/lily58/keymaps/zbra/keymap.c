cbcf8c026a820cb934d6b45781ed3ad79a3b8af0
jimmythebrawn <jimmybrawn.com@gmail.com>
2020-01-27 17:01:03 +0100

    shift and backspace makes del

--- keyboards/lily58/keymaps/zbra/keymap.c
+++ keyboards/lily58/keymaps/zbra/keymap.c
@@ -19,12 +19,14 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+static bool bsdel_mods = false;
 
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     LOWER,
     RAISE,
     ADJUST,
+    M_BSDEL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    M_BSDEL, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
@@ -195,6 +197,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
+        case M_BSDEL: {
+            uint8_t kc = KC_BSPC;
+
+            if (record->event.pressed) {
+                if (keyboard_report->mods) {
+                    kc = KC_DEL;
+                }
+                register_code (kc);
+                bsdel_mods = keyboard_report->mods;
+            }
+            else {
+                if (bsdel_mods) {
+                    kc = KC_DEL;
+                }
+                unregister_code (kc);
+            }
+        }
+        return false;
+        break;
         case QWERTY:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_QWERTY);
