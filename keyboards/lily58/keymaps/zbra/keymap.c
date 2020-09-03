0f369148f9323e0675b1860888cc272d0ac2c11f
Jimmy Brawn <jimmybrawn.com@gmail.com>
2020-09-03 10:15:40 +0200

    win

--- keyboards/lily58/keymaps/zbra/keymap.c
+++ keyboards/lily58/keymaps/zbra/keymap.c
@@ -19,17 +19,39 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
-
+#define _WINDOWS 4
 
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     LOWER,
     RAISE,
     ADJUST,
+    WINDOWS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* WINDOWS MT()
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  ~   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BCKSP|
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | OS   | Alt  | Ctrl | /Space  /       \Enter \  |Lower | Raise |Adjust |
+ *                   |      |      |      |/       /         \      \ |      |       |       |
+ *                   `-------------------''-------'           '------''---------------------'
+ */
 
+ [_WINDOWS] = LAYOUT( \
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                             KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,   KC_ENT,   MO(1),   MO(2),   TT(3) \
+),
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  ~   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BCKSP|
@@ -189,9 +211,9 @@ void iota_gfx_task_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
-#ifdef SSD1306OLED
-        // set_keylog(keycode, record);
-#endif
+        #ifdef SSD1306OLED
+                // set_keylog(keycode, record);
+        #endif
         // set_timelog();
     }
 
